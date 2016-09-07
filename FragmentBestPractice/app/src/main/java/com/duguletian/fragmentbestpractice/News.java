8cc2525c8a637477a1f0c3ca53516d94bc34b3ee
DuGuLeTian <lth1429081529@gmail>
2016-09-07 23:16:24 +0800

    first commit

---
+++ FragmentBestPractice/app/src/main/java/com/duguletian/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.duguletian.fragmentbestpractice;
+
+/**
+ * Created by duguletian on 2016/9/7.
+ */
+public class News {
+    private String title = "";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String content = "";
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ FragmentBestPractice/app/src/main/java/com/duguletian/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,85 @@
+package com.duguletian.fragmentbestpractice;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by duguletian on 2016/9/7.
+ */
+public class NewsTitleFragment extends Fragment {
+
+    private List<News> newsList;
+    private NewsAdapter adapter;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        newsList = getNews();   //初始化新闻数据
+        adapter = new NewsAdapter(context, R.layout.news_item, newsList);
+    }
+
+    private ListView newsTitleListView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        newsTitleListView = (ListView) view.findViewById(R.id.news_title_list_view);
+        newsTitleListView.setAdapter(adapter);
+        newsTitleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                News news = newsList.get(i);
+                if (isTwoPane) {
+                    //如果是双页模式，则刷新NewsContentFragment中的内容
+                    NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager()
+                            .findFragmentById(R.id.news_content_fragment);
+                    newsContentFragment.refresh(news.getTitle(), news.getContent());
+                } else {
+                    //如果是单页模式，则直接启动NewsContentActivity
+                    NewsContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent());
+                }
+            }
+        });
+        return view;
+    }
+
+    private boolean isTwoPane;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null) {
+            isTwoPane = true;   //可以找到news_content_layout布局时，为双页模式
+        } else {
+            isTwoPane = false;  //找不到news_content_layout布局时，为单页模式
+        }
+    }
+
+    private List<News> getNews() {
+        List<News> newsList = new ArrayList<News>();
+        News news1 = new News();
+        news1.setTitle("Succeed in College as a Learning Disabled Student");
+        news1.setContent("College freshmen will soon learn to live with a roommate, adjust to a new social scene " +
+                "and survive less-than-stellar dining hall food.Students with learning disabilities will face these " +
+                "transitions while also grappling with a few more hurdles.");
+        newsList.add(news1);
+
+        News news2 = new News();
+        news2.setTitle("Google Android exec poached by China's Xiaomi");
+        news2.setContent("China's Xiaomi has poached a key Google executive involved in the tech giant's Android " +
+                "phones, in a move seen as a coup for the rapidly growing Chinese smartphone maker.");
+        newsList.add(news2);
+        return newsList;
+    }
+}
